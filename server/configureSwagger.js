f5996ed41e5db952a697ba815cf03544ec5e6ffd
jeffreywescott <jeffrey@learnersguild.org>
2015-12-11 12:51:56 -0800

    customize Swagger API docs header

--- server/configureSwagger.js
+++ server/configureSwagger.js
@@ -1,16 +1,39 @@
+import fs from 'fs'
 import path from 'path'
+
+import Promise from 'bluebird'
 import swaggerTools from 'swagger-tools'
 import YAML from 'yamljs'
+import ejs from 'ejs'
+import fetch from 'node-fetch'
 
-// swaggerRouter configuration
-// const options = {
-//   swaggerUi: '/swagger.json',
-//   useStubs: process.env.NODE_ENV === 'development' ? true : false // Conditionally turn on stubs (mock mode)
-// }
 
 // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
 const swaggerDoc = YAML.load(path.join(__dirname, '../config/swagger-idm.yaml'))
 
+function replaceSwaggerUiHtml() {
+  const title = 'IDM API'
+  const logoUrl = 'https://lg-icons.herokuapp.com/favicon-32x32.png'
+  const customSwaggerUiTemplateFilename = path.join(__dirname, '..', 'public', 'swagger-docs.html.ejs')
+  const swaggerUiHtmlFilename = path.join(__dirname, '..', 'node_modules', 'swagger-tools', 'middleware', 'swagger-ui', 'index.html')
+
+  return new Promise((resolve) => {
+    fetch(process.env.ICONS_SERVICE_TAGS_API_URL)
+      .then((resp) => {
+        return resp.json()
+      }).then((tags) => {
+        return tags.join('\n        ')
+      }).then((iconsMetadataTagsHtml) => {
+        const templateData = fs.readFileSync(customSwaggerUiTemplateFilename).toString('utf-8')
+        const renderedTemplate = ejs.render(templateData, { title, logoUrl, iconsMetadataTagsHtml })
+        // console.log('renderedTemplate:', renderedTemplate)
+        console.log('writing file:', swaggerUiHtmlFilename)
+        fs.writeFileSync(swaggerUiHtmlFilename, renderedTemplate.toString())
+        console.log('wrote file:', swaggerUiHtmlFilename)
+        resolve(renderedTemplate)
+      })
+  })
+}
 
 export default function configureSwagger(app, next) {
   // Initialize the Swagger middleware
@@ -25,9 +48,12 @@ export default function configureSwagger(app, next) {
     // app.use(middleware.swaggerRouter(options))
 
     // Serve the Swagger documents and Swagger UI
-    app.use(middleware.swaggerUi())
+    replaceSwaggerUiHtml()
+      .then((templateData) => {
+        app.use(middleware.swaggerUi())
 
-    // Start the server
-    return next()
+        // Start the server
+        return next()
+      })
   })
 }
