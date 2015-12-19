47a6d0a75cf91d9480d446c9343686fd593de169
jeffreywescott <jeffrey@learnersguild.org>
2015-12-18 16:50:11 -0800

    remove mocks and use actual API

--- server/controllers/Users.js
+++ server/controllers/Users.js
@@ -1,18 +1,30 @@
-const mockUser = {
-  id: 'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee',
-  name: 'Mock User Name',
-  email: 'mockuser@example.com',
-}
+import {
+  find,
+  getById,
+} from '../dao/Users'
 
 
 export function findUsers(req, res) {
-  res.status(200).json([mockUser])
+  find(req.query)
+    .then((rows) => res.status(200).json(rows))
+    .catch((err) => {
+      res.status(500).json({ code: err.code, message: err.toString() })
+    })
 }
 
 export function getUserById(req, res) {
-  res.status(200).json(mockUser)
+  getById(req.swagger.params.id.value)
+    .then((result) => {
+      if (!result) {
+        res.status(404).json({ code: '404', message: 'Not Found' })
+      }
+      res.status(200).json(result)
+    })
+    .catch((err) => {
+      res.status(500).json({ code: err.code, message: err.toString() })
+    })
 }
 
 export function getCurrentUser(req, res) {
-  res.status(200).json(mockUser)
+  res.status(404).json({ code: '404', message: 'Not Found' })
 }
