76ff66b4f13afa362ef9a9f30801316bb3be897f
jeffreywescott <jeffrey@learnersguild.org>
2015-12-27 22:46:23 -0800

    get rid of the useless 'identity' reducer

--- common/reducers/index.js
+++ common/reducers/index.js
@@ -2,13 +2,8 @@ import { combineReducers } from 'redux'
 
 import { routeReducer } from 'redux-simple-router'
 
-function identity(state = {} /* , action */) {
-  return state
-}
-
 const rootReducer = combineReducers({
   routing: routeReducer,
-  identity,
 })
 
 export default rootReducer
