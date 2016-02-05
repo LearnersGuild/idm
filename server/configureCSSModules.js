101493a2e2eac7add992b288fdaf4297738a7f35
jeffreywescott <jeffrey@learnersguild.org>
2016-02-05 15:01:51 -0800

    CSS modules needed in production, also

--- server/configureCSSModules.js
+++ server/configureCSSModules.js
@@ -1,19 +1,14 @@
-
-// set up CSS modules when not in production
-
 export default function configureCSSModules() {
-  if (process.env.NODE_ENV !== 'production') {
-    const hook = require('css-modules-require-hook')
-    const sass = require('node-sass')
-    hook({
-      extensions: ['.scss'],
-      generateScopedName: '[name]__[local]__[hash:base64:5]',
-      preprocessCss: css => {
-        const result = sass.renderSync({
-          data: css
-        })
-        return result.css
-      }
-    })
-  }
+  const hook = require('css-modules-require-hook')
+  const sass = require('node-sass')
+  hook({
+    extensions: ['.scss'],
+    generateScopedName: '[name]__[local]__[hash:base64:5]',
+    preprocessCss: css => {
+      const result = sass.renderSync({
+        data: css
+      })
+      return result.css
+    }
+  })
 }
