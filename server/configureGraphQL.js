05e1e4dcab23c535c4ae40d293aea494b6c2c961
jeffreywescott <jeffrey@learnersguild.org>
2016-02-05 13:34:31 -0800

    use GraphiQL

--- server/configureGraphQL.js
+++ server/configureGraphQL.js
@@ -5,7 +5,7 @@ import rootSchema from './graphql/rootSchema'
 
 export default function configureGraphQL(app) {
   return new Promise(resolve => {
-    app.use('/graphql', graphqlHTTP({schema: rootSchema, pretty: true}))
+    app.use('/graphql', graphqlHTTP({schema: rootSchema, pretty: true, graphiql: true}))
     return resolve()
   })
 }
