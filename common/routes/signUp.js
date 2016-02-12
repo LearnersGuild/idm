6ec17656dcf63de1d4dfebd0817d6a123da76cd1
jeffreywescott <jeffrey@learnersguild.org>
2016-02-12 11:45:59 -0800

    refactor asynchronous route loading

--- common/routes/signUp.js
+++ common/routes/signUp.js
@@ -1,22 +1,6 @@
 /* eslint-disable no-undef */
 
-// This is terribly ugly, but seems to be the cleanest way I could find to
-// asynchronously load routes to support code splitting in the browser. The
-// loadRoute function is different depending on whether the route is being
-// loaded on the server (in which case a simple 'require' is used) or in
-// the browser (in which case it uses webpack 2.x's 'System.import').
-
-function getComponent(route) {
-  return async (location, cb) => {
-    const component = await System.import('../components/SignUpForm')
-    // That's right. We're actually wholesale replacing the asynchronous-
-    // loading getComponent function with the actual component. UGLY.
-    // But it works.
-    route.component = component
-    delete route.getComponent
-    cb(null, component)
-  }
-}
+import replaceComponent from './replaceComponent'
 
 const route = {
   path: 'sign-up',
@@ -24,7 +8,10 @@ const route = {
 if (__SERVER__) {
   route.component = require('../components/SignUpForm')
 } else {
-  route.getComponent = getComponent(route)
+  route.getComponent = async (location, cb) => {
+    const component = await System.import('../components/SignUpForm')
+    replaceComponent(route, component, cb)
+  }
 }
 
 export default route
