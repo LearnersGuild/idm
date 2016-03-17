f0cd1a4c1105604e64da196fa9a6aa04eda27e3b
jeffreywescott <jeffrey@learnersguild.org>
2016-03-17 12:05:50 -0700

    use component function

--- common/components/SignInButton.jsx
+++ common/components/SignInButton.jsx
@@ -1,32 +1,30 @@
-import React, {Component, PropTypes} from 'react'
+import React, {PropTypes} from 'react'
 
 import {Button} from 'react-toolbox/lib/button'
 
 import styles from './SignInUp.scss'
 
-export default class SignIn extends Component {
-  render() {
-    const {
-      location,
-      buttonLabel,
-    } = this.props
-    const redirectTo = (location && location.query) ? location.query.redirect : null
-    const signInGitHubHref = redirectTo ? `/auth/github?redirectTo=${redirectTo}` : '/auth/github'
-    return (
-      <Button
-        href={signInGitHubHref}
-        linkButton
-        raised
-        primary
-        style={styles.button}
-        >
-        <span className="socicon socicon-github button-icon"></span> {`${buttonLabel || 'Sign-in'} Using GitHub`}
-      </Button>
-    )
-  }
+export default function signInButton(props) {
+  const {
+    location,
+    buttonLabel,
+  } = props
+  const redirectTo = (location && location.query) ? location.query.redirect : null
+  const signInGitHubHref = redirectTo ? `/auth/github?redirectTo=${redirectTo}` : '/auth/github'
+  return (
+    <Button
+      href={signInGitHubHref}
+      linkButton
+      raised
+      primary
+      style={styles.button}
+      >
+      <span className="socicon socicon-github button-icon"></span> {`${buttonLabel || 'Sign-in'} Using GitHub`}
+    </Button>
+  )
 }
 
-SignIn.propTypes = {
+signInButton.propTypes = {
   location: PropTypes.shape({
     query: PropTypes.object.isRequired,
   }),
