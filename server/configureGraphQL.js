fab118e72953590cc14870c0e7fa445ec9597a3b
jeffreywescott <jeffrey@learnersguild.org>
2016-04-06 20:56:43 -0700

    allow CORS requests for LG services

--- server/configureGraphQL.js
+++ server/configureGraphQL.js
@@ -1,9 +1,17 @@
 import graphqlHTTP from 'express-graphql'
+import cors from 'cors'
 
 import rootSchema from './graphql/rootSchema'
 
 export default function configureGraphQL(app) {
-  app.use('/graphql', graphqlHTTP(req => ({
+  const corsOptions = {
+    origin: [
+      /\.learnersguild.org/,
+      /localhost/,
+    ],
+    exposedHeaders: ['LearnersGuild-JWT'],
+  }
+  app.use('/graphql', cors(corsOptions), graphqlHTTP(req => ({
     schema: rootSchema,
     rootValue: {currentUser: req.user},
     pretty: true,
