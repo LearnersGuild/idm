cb97515657275cd861a23ba7995f5ed65f7c0600
jeffreywescott <jeffrey@learnersguild.org>
2016-04-11 10:26:48 -0700

    log errors to sentry

--- server/server.js
+++ server/server.js
@@ -12,66 +12,73 @@ import raven from 'raven'
 import configureDevEnvironment from './configureDevEnvironment'
 import configureChangeFeeds from './configureChangeFeeds'
 
+const sentry = new raven.Client(process.env.SENTRY_SERVER_DSN)
+
 export function start() {
-  // error handling
-  raven.patchGlobal(process.env.SENTRY_SERVER_DSN)
+  try {
+    // error handling
+    raven.patchGlobal(process.env.SENTRY_SERVER_DSN)
 
-  const serverPort = parseInt(process.env.PORT, 10)
-  const baseUrl = process.env.APP_BASEURL
+    const serverPort = parseInt(process.env.PORT, 10)
+    const baseUrl = process.env.APP_BASEURL
 
-  const app = new Express()
-  const httpServer = http.createServer(app)
+    const app = new Express()
+    const httpServer = http.createServer(app)
 
-  // catch-all error handler
-  app.use((err, req, res, next) => {
-    const errCode = err.code || 500
-    const errType = err.type || 'Internal Server Error'
-    const errMessage = err.message || (process.env.NODE_ENV === 'production') ? err.toString() : err.stack
-    const errInfo = `<h1>${errCode} - ${errType}</h1><p>${errMessage}</p>`
-    console.error(err.stack)
-    res.status(500).send(errInfo)
-  })
+    // catch-all error handler
+    app.use((err, req, res, next) => {
+      const errCode = err.code || 500
+      const errType = err.type || 'Internal Server Error'
+      const errMessage = err.message || (process.env.NODE_ENV === 'production') ? err.toString() : err.stack
+      const errInfo = `<h1>${errCode} - ${errType}</h1><p>${errMessage}</p>`
+      console.error(err.stack)
+      res.status(500).send(errInfo)
+    })
 
-  if (process.env.NODE_ENV === 'development') {
-    configureDevEnvironment(app)
-  }
-
-  // Parse cookies.
-  app.use(cookieParser())
-
-  // Ensure secure connection in production.
-  if (process.env.NODE_ENV === 'production') {
-    /* eslint new-cap: [2, {"capIsNewExceptions": ["HTTPS"]}] */
-    app.use(enforceSecure.HTTPS({trustProtoHeader: true}))
-  }
-
-  // Use this middleware to server up static files
-  app.use(serveStatic(path.join(__dirname, '../dist')))
-  app.use(serveStatic(path.join(__dirname, '../public')))
-
-  // auth routes
-  app.use((req, res, next) => {
-    require('./auth')(req, res, next)
-  })
-
-  // GraphQL routes
-  app.use((req, res, next) => {
-    require('./graphql')(req, res, next)
-  })
-
-  // Default React application
-  app.get('*', (req, res, next) => {
-    require('./render').default(req, res, next)
-  })
-
-  // change feeds
-  configureChangeFeeds()
-
-  return httpServer.listen(serverPort, error => {
-    if (error) {
-      console.error(error)
-    } else {
-      console.info('🌍  Listening at %s', baseUrl)
+    if (process.env.NODE_ENV === 'development') {
+      configureDevEnvironment(app)
     }
-  })
+
+    // Parse cookies.
+    app.use(cookieParser())
+
+    // Ensure secure connection in production.
+    if (process.env.NODE_ENV === 'production') {
+      /* eslint new-cap: [2, {"capIsNewExceptions": ["HTTPS"]}] */
+      app.use(enforceSecure.HTTPS({trustProtoHeader: true}))
+    }
+
+    // Use this middleware to server up static files
+    app.use(serveStatic(path.join(__dirname, '../dist')))
+    app.use(serveStatic(path.join(__dirname, '../public')))
+
+    // auth routes
+    app.use((req, res, next) => {
+      require('./auth')(req, res, next)
+    })
+
+    // GraphQL routes
+    app.use((req, res, next) => {
+      require('./graphql')(req, res, next)
+    })
+
+    // Default React application
+    app.get('*', (req, res, next) => {
+      require('./render').default(req, res, next)
+    })
+
+    // change feeds
+    configureChangeFeeds()
+
+    return httpServer.listen(serverPort, error => {
+      if (error) {
+        console.error(error)
+      } else {
+        console.info('🌍  Listening at %s', baseUrl)
+      }
+    })
+  } catch (err) {
+    console.error(err.stack)
+    sentry.captureException(err)
+  }
 }
