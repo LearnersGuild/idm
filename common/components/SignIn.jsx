4d915e17ac489ca408c967d3383557ae0df33e8c
jeffreywescott <jeffrey@learnersguild.org>
2016-06-07 15:37:05 -0700

    don't lose track of query parameters when switching from sign-in to sign-up
    Parameters like redirect and responseType may be necessary as part of the new user sign-up flow. Be sure the propagate them through when the user clicks the sign-up link.

--- common/components/SignIn.jsx
+++ common/components/SignIn.jsx
@@ -3,21 +3,21 @@ import {Link} from 'react-router'
 
 import {Card} from 'react-toolbox/lib/card'
 
+import {buildURL} from '../util'
 import AuthButton from './AuthButton'
 
 import styles from './SignInUp.scss'
 
 export default function signIn(props) {
-  const {location} = props
-  const redirect = (location && location.query) ? location.query.redirect : null
-  const responseType = (location && location.query) ? location.query.responseType : null
+  const {location: {query: {redirect, responseType}}} = props
+  const signUpLink = buildURL('/sign-up', {redirect, responseType})
   return (
     <Card className={styles.card}>
       <div className={styles.cardContent}>
         <img className={styles.lgLogo} src="https://brand.learnersguild.org/assets/learners-guild-logo-black-250x149.png"/>
         <AuthButton label="Sign-in" redirect={redirect} responseType={responseType}/>
         <div className={styles.signUpLink} >
-          <Link to="/sign-up">Don't have an account? Sign-up.</Link>
+          <Link to={signUpLink}>Don't have an account? Sign-up.</Link>
         </div>
       </div>
     </Card>
