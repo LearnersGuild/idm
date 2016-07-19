d29ff29427e713ec9d7943f1271497325ad27fff
jeffreywescott <jeffrey@learnersguild.org>
2016-07-19 14:27:36 -0700

    trying to fix production outage

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import passport from 'passport'
+import raven from 'raven'
 
 import {cookieOptsJWT} from '@learnersguild/idm-jwt-auth/lib/utils'
 import {
@@ -9,6 +10,8 @@ import {
 } from '@learnersguild/idm-jwt-auth/lib/middlewares'
 
 import {configureAuthWithGitHub} from './github'
+
+const sentry = new raven.Client(process.env.SENTRY_SERVER_DSN)
 
 /* eslint new-cap: [2, {"capIsNewExceptions": ["Router"]}] */
 const app = new express.Router()
@@ -21,7 +24,18 @@ passport.deserializeUser((user, done) => done(null, user))
 // app configuration
 app.use(passport.initialize())
 app.use(addUserToRequestFromJWT)
-app.use(refreshUserFromIDMService)
+app.use((req, res, next) => {
+  refreshUserFromIDMService(req, res, err => {
+    if (err) {
+      // this is not enough to break things -- if we are unable to refresh the
+      // user from IDM, but our JWT is still valid, it's okay, so we won't
+      // allow this error to propagate beyond this point
+      console.warn('WARNING: unable to refresh user from IDM service:', err)
+      sentry.captureException(err)
+    }
+    next()
+  })
+})
 app.use(extendJWTExpiration)
 
 // sign-out
